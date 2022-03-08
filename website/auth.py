327639bfadbeb55b091d7e74b3d58973f5163b53
faithremo <faithmakumi@gmail.com>
2022-03-08 12:29:45 +0300

    in auth.py  validate the loginin details,whether they have entered the correct detail and if yes,you are let in and if not promted to enter the details again.

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,34 @@
-from nis import cat
-from unicodedata import category
+from tkinter import Y
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 auth = Blueprint("auth", __name__)
 
 
+
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category= 'success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category= 'error')
+                
+        else:
+            flash('Email does not exist.', category= 'error')
     
+            
     return render_template("login.html")
 
 
@@ -45,7 +61,7 @@ def sign_up():
             flash("email is invalid.", category='error')
             
         else:
-            new_user = User(email=email, username=username, password=password1)
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sah256'))
             db.session.add(new_user)
             db.session.commit()
             flash('User created!')

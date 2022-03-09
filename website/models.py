8be7362391607f0f639d0084ebf8a5db89d9840f
faithremo <faithmakumi@gmail.com>
2022-03-09 12:52:47 +0300

    on models.py,create a table that represents  pitches created

--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import false
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -9,3 +10,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    
+    class Pitch(db.model):
+        id = db.Column(db.Integer, primary_key=True)
+        text = db.column(db.Text, nullable=false)
+        date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+        author =db.column(db.Integer, db.ForeinKey('user.id', ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -13,4 +14,13 @@ def home():
 @views.route("/create-post", method=['GET' 'POST'])
 @login_required
 def create_pitch():
+    if request.method == "POST":
+        text = request.form.get('text')
+        
+        if not text:
+            flash('Pitch cannot be empty', category='error')
+            
+        else:
+            flash('pitch created!', category='success')   
+    
     return render_template('create_pitch.html', user=current_user)
